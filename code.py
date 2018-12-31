d432ec41530ede4dd329a4db7005d3ba2e61ef80
Thibault CORDIER <mr.thibault.cordier@gmail.com>
2018-12-31 12:15:38 +0100

    Add auto construction similarity graph for all similarity measures

--- code.py
+++ code.py
@@ -63,53 +63,98 @@ if download_genesis_ic :
 '''
 
 import networkx as nx
-import numpy as np
+
 from tqdm import tqdm
 from time import time
 
-noun_list = list(wn.all_synsets('n'))
-N_noun = len(noun_list)
+import numpy as np
+from numpy.random import choice
 
-t0 = time()
-t1 = time()
+from scipy.sparse import csr_matrix
+from scipy.sparse import dok_matrix
 
-nx_G = nx.Graph()
+# Definition of all WordNet noun synsets
+wn_all = list(wn.all_synsets('n'))
+N_all = len(wn_all)
 
-print("Add Nodes:")
-for i, synset in enumerate(noun_list):
-    nx_G.add_node(i)
+# Sample of all WordNet noun synsets
+N_sel = 5000
+wn_sel = choice(wn_all,N_sel,False)
 
-print("Add Edges:")
-'''
-for i1, synset1 in enumerate(noun_list):
-    if i1%round(5*N_noun/100) == 0 :
-      percent = 100.*i1/N_noun
-      print(percent,"%","Time :",time()-t1)
-      t1 = time()
-    for i2, synset2 in enumerate(noun_list):
-        if i2>=i1 :
-            #x_G.add_edge(i1, i2, weight=synset1.path_similarity(synset2))
-'''
-cur_noun_list = list()
-for i1, synset1 in enumerate(noun_list):
-    if i1%round(5*N_noun/100) == 0 :
-        percent = 100.*i1/N_noun
-        print(percent,"%","Time :",time()-t1)
-        t1 = time()
-    for i2, synset2 in enumerate(cur_noun_list):
-        nx_G.add_edge(i1, i2, weight=synset1.res_similarity(synset2, ic))
-    cur_noun_list.append(synset1)
+node_file = open("graph/wordnet.nodes", "w")
+for i, node in enumerate(wn_sel) :
+    node_file.write(str(i)+" "+node.name()+"\n")
+node_file.close()
 
-# synset1.path_similarity(synset2)    # Hirst and St-Onge Similarity
-# synset1.lch_similarity(synset2)     # Leacock-Chodorow Similarity
-# synset1.wup_similarity(synset2)     # Wu-Palmer Similarity
-# synset1.res_similarity(synset2, ic) # Resnik Similarity (brown_ic or genesis_ic)
-# synset1.jcn_similarity(synset2, ic) # Jiang-Conrath Similarity (brown_ic or genesis_ic)
-# synset1.lin_similarity(synset2, ic) # Lin Similarity (semcor_ic)
+# Definition of similarity graph
+sim_measures = ["path","lch","wup","res","jcn","lin"]
 
-print(time()-t0)
+for method in sim_measures :
 
-nx.write_weighted_edgelist(nx_G,'graph/wordnet.graph')
+    print("##############################")
+    print("Use method "+ method + ":")
+
+    # Definition of times
+    t_begin = time()
+    t_current = time()
+    t_cumul = time()-t_begin
+
+    nx_G = nx.Graph()
+    #A = dok_matrix((N_sel,N_sel))
+
+    print("Add Nodes ...")
+
+    for i, synset in enumerate(wn_sel):
+        nx_G.add_node(i, synset=synset)
+
+    print("Add Edges ...")
+    wn_visited = list()
+
+    for i1, synset1 in enumerate(wn_sel):
+
+        if i1%round(5*N_sel/100) == 0 and i1!=0 :
+            percent = 100.*i1/N_sel
+            t_step = time()-t_current
+            t_cumul += t_step
+            t_final = 400*t_cumul/(np.sum(np.arange(1,1+percent/5)*2-1))
+            print(percent,"%","Time Step :",t_step,"Time Cumul :",t_cumul,"Time Final :",t_final)
+            t_current = time()
+
+        for i2, synset2 in enumerate(wn_visited):
+
+            # n = 5000
+            # synset1.path_similarity(synset2)    # Hirst and St-Onge Similarity >1h
+            # synset1.lch_similarity(synset2)     # Leacock-Chodorow Similarity >1h10min
+            # synset1.wup_similarity(synset2)     # Wu-Palmer Similarity >1h20min
+            # synset1.res_similarity(synset2, ic) # Resnik Similarity (brown_ic or genesis_ic) 130 sec
+            # synset1.jcn_similarity(synset2, ic) # Jiang-Conrath Similarity (brown_ic or genesis_ic) 150 sec
+            # synset1.lin_similarity(synset2, ic) # Lin Similarity (semcor_ic) 130 sec
+
+            if method == "path" :
+                sim = synset1.path_similarity(synset2)
+            elif method == "lch" :
+                sim = synset1.lch_similarity(synset2)
+            elif method == "wup" :
+                sim = synset1.wup_similarity(synset2)
+            elif method == "res" :
+                sim = synset1.res_similarity(synset2, ic)
+            elif method == "jcn" :
+                sim = synset1.jcn_similarity(synset2, ic)
+            elif method == "lin" :
+                sim = synset1.lin_similarity(synset2, ic)
+            nx_G.add_edge(i1, i2, weight=sim)
+
+        wn_visited.append(synset1)
+
+    # Print total time
+    print("Total Time :", time()-t_begin)
+
+    # Save similarity graph
+    print("Save Similarity Graph ...")
+    t_begin = time()
+    #nx_G = nx.from_scipy_sparse_matrix(A)
+    nx.write_weighted_edgelist(nx_G,'graph/wordnet_' + method + '.graph')
+    print("Total Time :", time()-t_begin)
 
 '''
 ###################################################################################################
